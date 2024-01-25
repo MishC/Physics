1507ed42e65816f385f6444c4701ac2bb20c10b8
MishC <michaela.chovancova@gmail.com>
2024-01-25 20:01:35 +0100

    Update black_body_radiation.m
    don't need element wise with scalar

--- black_body_radiation.m
+++ black_body_radiation.m
@@ -1,12 +1,12 @@
-c0=2.997*10.^8;% m/s speed of light in vaccum
-h=6.625*10.^-34;% J.s Planck constant 
-k=1.38*10.^-23;%  T/K Boltzmann constant
+c0=2.997*10^8;% m/s speed of light in vaccum
+h=6.625*10^-34;% J.s Planck constant 
+k=1.38*10^-23;%  T/K Boltzmann constant
 n=1;%refravtive index of the medium .
 step=  2.8429e-007;
-lambda=[10.^-7:step:2*10.^-5];
+lambda=[10^-7:step:2*10^-5];
 T=1;%  Temperature initiatlisation
 for i=300:50:800
-    Mi=(2*pi.*h.*(c0.^2))./((n.^2).*(lambda.^5).*(exp((h.*c0)./(n.*k.*T.*lambda.*i))-1));
+    Mi=(2*pi*h*(c0^2))./((n^2).*(lambda.^5).*(exp((h*c0)./((n*k*T).*lambda.*i))-1));
 plot(lambda,Mi)
 hold on
 grid
